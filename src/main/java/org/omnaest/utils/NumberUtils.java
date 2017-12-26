887a71f85a553c6d8d7074c9631d68c89e538e05
Omnaest <omnaest@web.de>
2017-12-26 13:57:25 +0100

    adjusted NumberUtils javadoc

--- src/main/java/org/omnaest/utils/NumberUtils.java
+++ src/main/java/org/omnaest/utils/NumberUtils.java
@@ -24,109 +24,160 @@ import java.util.function.Function;
 
 public class NumberUtils
 {
-	public static interface NumberFormatter
-	{
-		public NumberFormatter forLocale(Locale locale);
+    /**
+     * Allows to format {@link Double} and {@link Float} {@link Number}s<br>
+     * <br>
+     * Example:<br>
+     * 
+     * <pre>
+     * String result = NumberUtils.formatter()
+     *                            .withPercentage()
+     *                            .format(0.05);
+     * </pre>
+     * 
+     * @author omnaest
+     */
+    public static interface NumberFormatter
+    {
+        /**
+         * Sets the {@link Locale} for the {@link NumberFormatter}
+         * 
+         * @param locale
+         * @return
+         */
+        public NumberFormatter forLocale(Locale locale);
 
-		public String format(double value);
+        /**
+         * Formats the given {@link Double} value
+         * 
+         * @param value
+         * @return
+         */
+        public String format(double value);
 
-		public String format(float value);
+        /**
+         * Formates the given {@link Float} value
+         * 
+         * @param value
+         * @return
+         */
+        public String format(float value);
 
-		public NumberFormatter withMinimumFractionDigits(int minimumFractionDigits);
+        public NumberFormatter withMinimumFractionDigits(int minimumFractionDigits);
 
-		public NumberFormatter withMaximumFractionDigits(int maximumFractionDigits);
+        public NumberFormatter withMaximumFractionDigits(int maximumFractionDigits);
 
-		public NumberFormatter withThousandSeparator();
+        public NumberFormatter withThousandSeparator();
 
-		public NumberFormatter withPercentage();
-	}
+        /**
+         * Defines the render syntax to be e.g. like "0.1%"
+         * 
+         * @return
+         */
+        public NumberFormatter withPercentage();
+    }
 
-	public static NumberFormatter formatter()
-	{
-		return new NumberFormatter()
-		{
-			private Locale							locale					= Locale.US;
-			private Integer							minimumFractionDigits	= null;
-			private Integer							maximumFractionDigits	= null;
-			private boolean							useThousandSeparator	= false;
-			private Function<Locale, NumberFormat>	formatterFactory		= locale ->
-																			{
-																				return locale != null ? NumberFormat.getNumberInstance(locale)
-																						: NumberFormat.getNumberInstance();
-																			};
+    /**
+     * Returns a {@link NumberFormatter}
+     * 
+     * @return
+     */
+    public static NumberFormatter formatter()
+    {
+        return new NumberFormatter()
+        {
+            private Locale                         locale                = Locale.US;
+            private Integer                        minimumFractionDigits = null;
+            private Integer                        maximumFractionDigits = null;
+            private boolean                        useThousandSeparator  = false;
+            private Function<Locale, NumberFormat> formatterFactory      = locale ->
+                                                                         {
+                                                                             return locale != null ? NumberFormat.getNumberInstance(locale)
+                                                                                     : NumberFormat.getNumberInstance();
+                                                                         };
 
-			@Override
-			public NumberFormatter withThousandSeparator()
-			{
-				this.useThousandSeparator = true;
-				return this;
-			}
+            @Override
+            public NumberFormatter withThousandSeparator()
+            {
+                this.useThousandSeparator = true;
+                return this;
+            }
 
-			@Override
-			public NumberFormatter withPercentage()
-			{
-				this.formatterFactory = locale -> locale != null ? NumberFormat.getPercentInstance(locale) : NumberFormat.getPercentInstance();
-				if (this.maximumFractionDigits == null)
-				{
-					this.maximumFractionDigits = 2;
-				}
-				return this;
-			}
+            @Override
+            public NumberFormatter withPercentage()
+            {
+                this.formatterFactory = locale -> locale != null ? NumberFormat.getPercentInstance(locale) : NumberFormat.getPercentInstance();
+                if (this.maximumFractionDigits == null)
+                {
+                    this.maximumFractionDigits = 2;
+                }
+                return this;
+            }
 
-			@Override
-			public NumberFormatter forLocale(Locale locale)
-			{
-				this.locale = locale;
-				return this;
-			}
+            @Override
+            public NumberFormatter forLocale(Locale locale)
+            {
+                this.locale = locale;
+                return this;
+            }
 
-			@Override
-			public NumberFormatter withMinimumFractionDigits(int minimumFractionDigits)
-			{
-				this.minimumFractionDigits = minimumFractionDigits;
-				return this;
-			}
+            @Override
+            public NumberFormatter withMinimumFractionDigits(int minimumFractionDigits)
+            {
+                this.minimumFractionDigits = minimumFractionDigits;
+                return this;
+            }
 
-			@Override
-			public NumberFormatter withMaximumFractionDigits(int maximumFractionDigits)
-			{
-				this.maximumFractionDigits = maximumFractionDigits;
-				return this;
-			}
+            @Override
+            public NumberFormatter withMaximumFractionDigits(int maximumFractionDigits)
+            {
+                this.maximumFractionDigits = maximumFractionDigits;
+                return this;
+            }
 
-			@Override
-			public String format(double value)
-			{
-				return this	.createNumberFormatInstance()
-							.format(value);
-			}
+            @Override
+            public String format(double value)
+            {
+                return this.createNumberFormatInstance()
+                           .format(value);
+            }
 
-			private NumberFormat createNumberFormatInstance()
-			{
-				NumberFormat retval = this.formatterFactory.apply(this.locale);
+            private NumberFormat createNumberFormatInstance()
+            {
+                NumberFormat retval = this.formatterFactory.apply(this.locale);
 
-				retval.setMinimumFractionDigits(ObjectUtils.defaultIfNull(this.minimumFractionDigits, 0));
-				retval.setMaximumFractionDigits(ObjectUtils.defaultIfNull(this.maximumFractionDigits, 20));
-				retval.setGroupingUsed(this.useThousandSeparator);
-				return retval;
-			}
+                retval.setMinimumFractionDigits(ObjectUtils.defaultIfNull(this.minimumFractionDigits, 0));
+                retval.setMaximumFractionDigits(ObjectUtils.defaultIfNull(this.maximumFractionDigits, 20));
+                retval.setGroupingUsed(this.useThousandSeparator);
+                return retval;
+            }
 
-			@Override
-			public String format(float value)
-			{
-				return this	.createNumberFormatInstance()
-							.format(value);
-			}
-		};
-	}
+            @Override
+            public String format(float value)
+            {
+                return this.createNumberFormatInstance()
+                           .format(value);
+            }
+        };
+    }
 
-	/**
-	 * @see org.apache.commons.lang3.math.NumberUtils#toDouble(String)
-	 * @param value
-	 * @return
-	 */
-	public static double toDouble(String value)
-	{
-		return org.apache.commons.lang3.math.NumberUtils.toDouble(value);
-	}
+    /**
+     * @see org.apache.commons.lang3.math.NumberUtils#toDouble(String)
+     * @param str
+     * @return
+     */
+    public static double toDouble(String str)
+    {
+        return org.apache.commons.lang3.math.NumberUtils.toDouble(str);
+    }
+
+    /**
+     * @see org.apache.commons.lang3.math.NumberUtils#toInt(String)
+     * @param str
+     * @return
+     */
+    public static int toInt(String str)
+    {
+        return org.apache.commons.lang3.math.NumberUtils.toInt(str);
+    }
 }
