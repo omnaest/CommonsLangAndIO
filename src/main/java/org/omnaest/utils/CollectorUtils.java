32f87848af772048ce7ebf587948ca1e219f42c1
Omnaest <omnaest@web.de>
2018-01-12 19:09:21 +0100

    improved CollectorUtils

--- src/main/java/org/omnaest/utils/CollectorUtils.java
+++ src/main/java/org/omnaest/utils/CollectorUtils.java
@@ -1,6 +1,7 @@
 package org.omnaest.utils;
 
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -15,5 +16,10 @@ public class CollectorUtils
     public static <K, V, VR> Collector<Map.Entry<K, V>, ?, Map<K, VR>> toValueMappedMap(Function<Map.Entry<K, V>, VR> valueMapper)
     {
         return Collectors.toMap(entry -> entry.getKey(), valueMapper);
+    }
+
+    public static <K, V> Collector<Map.Entry<K, V>, ?, Map<K, V>> appendToMap(TreeMap<K, V> treeMap)
+    {
+        return Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(), (e1, e2) -> e1, () -> treeMap);
     }
 }
